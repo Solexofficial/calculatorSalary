40f32e41c546d24038270dd174e8aff4c8b39d56
solexofficial <solexofficial@yandex.ru>
2021-03-06 14:30:26 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+//income inputs
+const incomeSalary = document.getElementById('income-salary'),
+      incomeFreelance = document.getElementById('income-freelance'),
+      incomeExtra1 = document.getElementById('income-extra-1'),
+      incomeExtra2 = document.getElementById('income-extra-2');
+//costs inputs
+const costsFlat = document.getElementById('costs-flat'),
+      costsHouseServices = document.getElementById('costs-house-services'),
+      costsTransport = document.getElementById('costs-transport'),
+      costsCredit = document.getElementById('costs-credit');
+
+//total inputs
+const totalMonthInput = document.getElementById('total-month'),
+      totalDayInput = document.getElementById('total-day'),
+      totalYearInput = document.getElementById('total-year');
+
+let totalMonth, totalDay, totalYear;
+
+//money box
+const moneyBoxRange = document.getElementById('money-box-range'),
+      accumulationInput = document.getElementById('accumulation'),
+      spend = document.getElementById('spend');
+
+let accumulation = 0;
+let totalPrecents = 0;
+
+
+
